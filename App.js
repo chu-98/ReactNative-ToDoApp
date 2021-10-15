998b5193b438184c057086378259f3dd84ae006f
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-15 10:51:23 +0900

    feat: colors.js + Touchable

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          underlayColor="#DDDDDD"
+          onPress={() => console.log("Pressed!")}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -14,8 +31,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 40,
+    fontWeight: "600",
+    color: "white",
   },
 });
---
+++ colors.js
@@ -0,0 +1,4 @@
+export const theme = {
+  bg: "black",
+  grey: "#5C5C60",
+};
